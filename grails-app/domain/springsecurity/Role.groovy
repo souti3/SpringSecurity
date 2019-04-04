1d6e933b5bd028c366cf009004458499d65b970b
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-04-04 12:18:10 +0200

    Get rid of 'GrailsCompileStatic' as they bring some red into the annotated files

--- grails-app/domain/springsecurity/Role.groovy
+++ grails-app/domain/springsecurity/Role.groovy
@@ -2,22 +2,20 @@ package springsecurity
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
-import grails.compiler.GrailsCompileStatic
 
-@GrailsCompileStatic
-@EqualsAndHashCode(includes='authority')
-@ToString(includes='authority', includeNames=true, includePackage=false)
+@EqualsAndHashCode(includes = 'authority')
+@ToString(includes = 'authority', includeNames = true, includePackage = false)
 class Role implements Serializable {
 
-	private static final long serialVersionUID = 1
+    private static final long serialVersionUID = 1
 
-	String authority
+    String authority
 
-	static constraints = {
-		authority nullable: false, blank: false, unique: true
-	}
+    static constraints = {
+        authority nullable: false, blank: false, unique: true
+    }
 
-	static mapping = {
-		cache true
-	}
+    static mapping = {
+        cache true
+    }
 }
