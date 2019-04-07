7b5e0fa5e5044ed3aacfc011d2fc8a9e7d04901a
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-04-07 16:47:05 +0200

    Add 'passwordEncoder(PlaintextPasswordEncoder)'

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,7 @@
 import springsecurity.UserPasswordEncoderListener
+
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+    // passwordEncoder(PlaintextPasswordEncoder)
 }
