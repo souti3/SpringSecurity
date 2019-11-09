82d576fac886f1cbb240b6229e4b77f595aa3bbb
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-11-09 12:52:43 +0100

    Migrate master to Grails 4

---
+++ src/integration-test/resources/GebConfig.groovy
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+
+environments {
+
+    // run via “./gradlew -Dgeb.env=chrome iT”
+    chrome {
+        driver = { new ChromeDriver() }
+    }
+
+    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
+    chromeHeadless {
+        driver = {
+            ChromeOptions o = new ChromeOptions()
+            o.addArguments('headless')
+            new ChromeDriver(o)
+        }
+    }
+}
